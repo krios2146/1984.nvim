be231f778607f130fa66f47369c0d9fb90110f91
Vladimir Kidyaev <krios2146@gmail.com>
2024-11-03 02:42:25 +0700

    fix: rename theme files

--- colors/1984.lua
+++ colors/1984.lua
@@ -1,6 +1,6 @@
 -- You probably always want to set this in your vim file
-vim.opt.background = "dark"
-vim.g.colors_name = "1984.nvim"
+vim.opt.background = 'dark'
+vim.g.colors_name = '1984'
 
 -- By setting our module to nil, we clear lua's cache,
 -- which means the require ahead will *always* occur.
@@ -14,7 +14,7 @@ vim.g.colors_name = "1984.nvim"
 --
 -- The performance impact of this call can be measured in the hundreds of
 -- *nanoseconds* and such could be considered "production safe".
-package.loaded["lush_theme.1984.nvim"] = nil
+package.loaded['lush_theme.1984'] = nil
 
 -- include our theme file and pass it to lush to apply
-require("lush")(require("lush_theme.1984.nvim"))
+require 'lush'(require 'lush_theme.1984')
